a27fc9e599054b46d18dccdc72159917938be52b
Hyeonjong <nianelo4@gmail.com>
2023-07-23 23:36:02 +0900

    refactor: 모든 scoped style을 tailwindcss 방식으로 재작성

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,24 @@
+const plugin = require('tailwindcss/plugin');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: '#7c3aed',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(({ addUtilities, theme }) => {
+      addUtilities({
+        '.bg-gradient-accent': {
+          'background-image': `linear-gradient(45deg, ${
+            theme('colors').accent
+          }, #da62c4 30%, white 60%)`,
+        },
+      });
+    }),
+  ],
 };
